6b8a7c23cc1a9a62fe164b3e50c5c852d4fed1a2
TukadiyaRaju <raju.tukadiya@gmail.com>
2022-12-20 23:12:33 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
-  // code here
+  let gridStyle = {
+    display: "grid",
+    gap: "10px",
+    alignContent: "center",
+    justifyContent: "center",
+    gridTemplateColumns: "1fr 1fr",
+    gridTemplateRows: "1fr 1fr",
+    margin: "0 auto",
+  };
+
+  let imageStyle = {
+    maxHeight: "400px",
+    margin: "0 auto",
+    minWidth: "100px",
+  };
+  let data = imageData();
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}> Kalvium Gallery </h2>
+      <div style={gridStyle} id="grid">
+        {data.map((e) => {
+          return (
+            <img style={imageStyle} className="image" src={e.img} id={e.id} />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/AppClass.js
+++ src/AppClass.js
@@ -1,30 +1,64 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  gridStyle = {
+    display: "grid",
+    gap: "10px",
+    alignContent: "center",
+    justifyContent: "center",
+    gridTemplateColumns: "1fr 1fr",
+    gridTemplateRows: "1fr 1fr",
+    margin: "0 auto",
+  };
+
+  imageStyle = {
+    maxHeight: "400px",
+    margin: "0 auto",
+    minWidth: "100px",
+  };
+
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
 
-  // code here
+  render() {
+    let data = this.imageData();
+    return (
+      <>
+        <h2 style={{ textAlign: "center" }}> Kalvium Gallery </h2>
+        <div style={this.gridStyle} id="grid">
+          {data.map((e) => {
+            return (
+              <img
+                style={this.imageStyle}
+                className="image"
+                src={e.img}
+                id={e.id}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
